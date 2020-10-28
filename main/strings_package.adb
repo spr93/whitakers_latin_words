0e1d96c3e12924ff7394e1e5c65b931ddf644e99
sean rodriguez <42303133+spr93@users.noreply.github.com>
2020-10-27 23:37:20 -0700

    replace several functions w standard library functions

--- main/strings_package.adb
+++ main/strings_package.adb
@@ -13,109 +13,13 @@ with LIST_PACKAGE;
             return A; end if; 
          return B;
       end MAX;
-   
+
       function MIN(A, B : in INTEGER) return INTEGER is
       begin
          if A <= B  then 
             return A; end if; 
          return B;
       end MIN;
-   
-
-      function LOWER_CASE(C : in CHARACTER) return CHARACTER is
-      begin
-         if C in 'A'..'Z'  then
-            return CHARACTER'VAL(CHARACTER'POS(C) + 32);
-         else
-            return C;
-         end if;
-      end LOWER_CASE;
-   
-      function LOWER_CASE(S : in STRING) return STRING is
-         T : STRING(S'RANGE);
-      begin
-         for I in S'RANGE  loop
-            T(I) := LOWER_CASE(S(I));
-         end loop;
-         return T;
-      end LOWER_CASE;
-   
-   
-      function UPPER_CASE(C : in CHARACTER) return CHARACTER is
-      begin
-         if C in 'a'..'z'  then
-            return CHARACTER'VAL(CHARACTER'POS(C) - 32);
-         else
-            return C;
-         end if;
-      end UPPER_CASE;
-   
-      function UPPER_CASE(S : in STRING) return STRING is
-         T : STRING(S'RANGE);
-      begin
-         for I in S'RANGE  loop
-            T(I) := UPPER_CASE(S(I));
-         end loop;
-         return T;
-      end UPPER_CASE;
-   
-   
-      function TRIM(SOURCE : in STRING;
-                    SIDE   : in TRIM_END := BOTH) return STRING is
-      --  Removes leading and trailing blanks and returns a STRING staring at 1
-      --  For a string of all blanks as input it returns NULL_STRING
-         T : STRING(1..SOURCE'LENGTH) := SOURCE;
-         FIRST: NATURAL := SOURCE'FIRST; 
-         LAST : NATURAL := SOURCE'LAST;
-
-      begin
-         if SIDE /= RIGHT  then
-            FIRST := SOURCE'LAST + 1;
-            for I in SOURCE'RANGE  loop
-               if SOURCE(I) /= ' '  then
-                  FIRST := I;
-                  exit;
-               end if;
-            end loop;
-         else
-            FIRST := SOURCE'FIRST;
-         end if;
-      
-         if SIDE /= LEFT  then
-            LAST := SOURCE'FIRST - 1;
-            for I in reverse SOURCE'RANGE  loop
-               if SOURCE(I) /= ' '  then
-                  LAST := I;
-                  exit;
-               end if;
-            end loop;
-         else
-            LAST := SOURCE'LAST;
-         end if;
-      
-         if FIRST > LAST  then
-            return NULL_STRING;
-         else
-            T(1..LAST-FIRST+1) := SOURCE(FIRST..LAST);
-            return T(1..LAST-FIRST+1);
-         end if;
-      end TRIM;        
-   
-   
-      function HEAD(SOURCE : in STRING; 
-                    COUNT  : in NATURAL; 
-                    PAD    : in CHARACTER := ' ') return STRING is
-      --  Truncates or fills a string to exactly N in length
-         T : STRING(1..COUNT) := (others => ' ');
-      begin
-         if COUNT < SOURCE'LENGTH  then
-            T(1..COUNT) := SOURCE(SOURCE'FIRST..SOURCE'FIRST+COUNT-1);
-         else
-            T(1..SOURCE'LENGTH) := SOURCE(SOURCE'FIRST..SOURCE'LAST);
-         end if;
-         return T;
-      end HEAD; 
-   
    
       procedure GET_NON_COMMENT_LINE(F : in TEXT_IO.FILE_TYPE; 
                                      S : out STRING; LAST : out INTEGER) is
@@ -159,7 +63,7 @@ with LIST_PACKAGE;
          W_Line : Wide_String := Ada.Wide_Text_IO.Get_Line;
 
          T_Line : String := Ada.Characters.Conversions.To_String (    
-                          Ada.Wide_Characters.Handling.To_Basic(W_Line));
+                            Ada.Wide_Characters.Handling.To_Basic(W_Line));
          
    begin
       null;
--- main/strings_package.ads
+++ main/strings_package.ads
@@ -1,36 +1,41 @@
-   with TEXT_IO;
-   package STRINGS_PACKAGE is
-      type TRIM_END is (LEFT, RIGHT, BOTH);
+with Text_IO;
+with Ada.Characters.Handling; 
+with Ada.Strings;             use Ada.Strings;
+with Ada.Strings.Fixed;
+
+package STRINGS_PACKAGE is
+
+     NULL_STRING                :   constant STRING(2..1)     
+                                := (others => ' ');
    
-      NULL_STRING : constant STRING(2..1) := (others => ' ');
+     function MAX(A, B          : in INTEGER)    return INTEGER;
+     function MIN(A, B          : in INTEGER)    return INTEGER;
+
+     function Upper_Case(Item   : in Character)  return Character renames Ada.Characters.Handling.To_Upper;
+     function Upper_Case(Item   : in String)     return String    renames Ada.Characters.Handling.To_Upper;
+  
+     function Lower_Case(Item   : in Character)  return Character renames Ada.Characters.Handling.To_Lower;
+     function Lower_Case(Item   : in String)     return String    renames Ada.Characters.Handling.To_Lower;
    
+     function TRIM    (SOURCE   : in STRING;
+                       SIDE     : in TRIM_END  := BOTH)          
+                                                  return STRING   renames Ada.Strings.Fixed.Trim;
+
+     function HEAD(SOURCE       : in STRING; 
+                    COUNT       : in NATURAL; 
+                      PAD       : in CHARACTER := ' ') return STRING renames Ada.Strings.Fixed.Head;  
    
-      function MAX(A, B : in INTEGER) return INTEGER;
-      function MIN(A, B : in INTEGER) return INTEGER;
+     procedure GET_NON_COMMENT_LINE (F         :  in TEXT_IO.FILE_TYPE; 
+                                     S         :  out STRING; 
+                                     LAST      :  out INTEGER);
    
+     INPUT_LINE_LENGTH          : Integer      := 2_500;
+
+     procedure GET_UNICODE(LINE : in out String; 
+                           L    : in out Integer);
    
-      function LOWER_CASE(C : in CHARACTER) return CHARACTER;
-      function LOWER_CASE(S : in STRING) return STRING;
+     pragma Inline(GET_UNICODE);
    
-      function UPPER_CASE(C : in CHARACTER) return CHARACTER;
-      function UPPER_CASE(S : in STRING) return STRING;
-   
-      function TRIM(SOURCE : in STRING;
-                    SIDE   : in TRIM_END := BOTH) return STRING;
-   --  Equivalent to Ada.Strings.Fixed.Trim(Source, Both);
-   
-      function HEAD(SOURCE : in STRING; 
-                    COUNT  : in NATURAL; 
-                    PAD    : in CHARACTER := ' ') return STRING;  
-   
-   
-      procedure GET_NON_COMMENT_LINE(F : in TEXT_IO.FILE_TYPE; 
-                                     S : out STRING; LAST : out INTEGER);
-   
-   
-   INPUT_LINE_LENGTH : Integer := 2_500;
-   procedure GET_UNICODE (LINE : in out String; L : in out Integer);
-   pragma Inline(GET_UNICODE);
-   pragma Wide_Character_Encoding(UTF8);
-   
-   end STRINGS_PACKAGE;  
+     pragma Wide_Character_Encoding(UTF8);
+
+end STRINGS_PACKAGE;  
