ff2748197006b7a15034ab903347a07861d70cfe
sean rodriguez <42303133+spr93@users.noreply.github.com>
2020-12-18 20:50:38 -0800

    formatting only

--- main/config.ads
+++ main/config.ads
@@ -1,21 +1,20 @@
 package CONFIG is
-
    
-  OUTPUT_SCREEN_SIZE : INTEGER := 20;
+  OUTPUT_SCREEN_SIZE : INTEGER            := 20;
 
   type CONFIGURATION_TYPE is (DEVELOPER_VERSION, USER_VERSION, ONLY_MEANINGS); 
                               
-  CONFIGURATION : CONFIGURATION_TYPE := DEVELOPER_VERSION;
+  CONFIGURATION      : CONFIGURATION_TYPE := DEVELOPER_VERSION;
 
   type METHOD_TYPE is (INTERACTIVE, COMMAND_LINE_INPUT, COMMAND_LINE_FILES);
 
-  METHOD : METHOD_TYPE := INTERACTIVE;
+  METHOD             : METHOD_TYPE        := INTERACTIVE;
 
   type LANGUAGE_TYPE is (LATIN_TO_ENGLISH, ENGLISH_TO_LATIN);
 
-  LANGUAGE : LANGUAGE_TYPE := LATIN_TO_ENGLISH;
+  LANGUAGE           : LANGUAGE_TYPE      := LATIN_TO_ENGLISH;
 
-  SUPPRESS_PREFACE : BOOLEAN := FALSE;  
+  SUPPRESS_PREFACE   : BOOLEAN := FALSE;  
   
   Type CL_Arguments_Type is (READ_ONLY, 
                              NO_FILES,
@@ -30,6 +29,6 @@ package CONFIG is
    
    Null_CL_Arguments : constant CL_Arguments_Array_Type := (others => False);
    
-   CL_Arguments : CL_Arguments_Array_Type := Null_CL_Arguments;
+   CL_Arguments      : CL_Arguments_Array_Type := Null_CL_Arguments;
    
 end CONFIG;
