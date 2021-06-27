8163bb5a262dca5785283650c9bed6ce84da569a
sean rodriguez <42303133+spr93@users.noreply.github.com>
2021-06-26 21:33:31 -0700

    new help routines
    also implements PAUSE feature in help

--- main/words_help.adb
+++ main/words_help.adb
@@ -1,57 +1,140 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;          use Ada.Text_IO;
+with CONFIG;               use CONFIG;
+with WORD_PACKAGE;
+with DEVELOPER_PARAMETERS; use DEVELOPER_PARAMETERS;
+with STRINGS_PACKAGE;      use STRINGS_PACKAGE;
 
 
-package body words_help is
+package body Words_Help is
+
+   ---------
+   -- PUT --
+   ---------
+
+   procedure PUT (HELP : in Main_Help_Type; Output : in Text_IO.File_Type) is
+
+      Need_To_Reset : Boolean := False;
+
+   begin
+      New_Line;
+
+      for Line_Counter in HELP'Range loop
+
+         if WORDS_MDEV (PAUSE_IN_SCREEN_OUTPUT) and
+           Line_Counter > OUTPUT_SCREEN_SIZE and
+           (Line_Counter mod (OUTPUT_SCREEN_SIZE + 1)) = 0
+         then
+            if Need_To_Reset and WORDS_MODE (DO_ANSI_FORMATTING) then
+               Format (Output, RESET);
+            end if;
+
+            WORD_PACKAGE.PAUSE (Output);
+
+            if Need_To_Reset and WORDS_MODE (DO_ANSI_FORMATTING) then
+               Format (Output, BOLD);
+            end if;
+         end if;
+
+         if HELP (Line_Counter) = Skip_Next then
+            New_Line;
+
+         elsif HELP (Line_Counter) = Underline_Next then
+            if WORDS_MODE (DO_ANSI_FORMATTING) then
+               Format (Output, UNDERLINE);
+            end if;
+            Need_To_Reset := True;
+         elsif HELP (Line_Counter) = Bold_Next then
+            if WORDS_MODE (DO_ANSI_FORMATTING) then
+               Format (Output, BOLD);
+               Need_To_Reset := True;
+            else
+               Need_To_Reset := False;
+            end if;
+         else
+
+            for I in HELP (Line_Counter)'First .. HELP (Line_Counter)'Last loop
+
+               if (HELP (Line_Counter) (I) = '|') then
+                  if Need_To_Reset then
+                     case WORDS_MODE (DO_ANSI_FORMATTING) is
+                        when True =>
+                           Format (Output, RESET);
+
+                        when False =>
+                           New_Line;
+                           for J in HELP'First .. (I - 1) loop
+                              Put ('-');
+                           end loop;
+                     end case;
+
+                     Need_To_Reset := False;
+                  end if;
+                  exit;
+
+               else
+                  Put (HELP (Line_Counter) (I));
+
+                  if (I = HELP (Line_Counter)'last) and
+                    WORDS_MODE (DO_ANSI_FORMATTING)
+                  then
+                     Format (Output, RESET);
+                  end if;
+               end if;
+
+        end loop;
+
+        New_Line;
+
+         end if;
+
+         --  end loop;
+      end loop;
+
+      New_Line;
+   end PUT;
 
    ---------------
    -- SHOW_HELP --
    ---------------
 
-   procedure PUT(HELP : in Main_HELP_TYPE) is
-  begin
-    NEW_LINE;
-    for I in HELP'FIRST..HELP'LAST  loop
-      PUT_LINE(HELP(I));
-    end loop;
-    NEW_LINE;
-   end PUT;
+   procedure SHOW_HELP (Line : in String; Output : in Text_IO.File_Type) is
 
-   procedure SHOW_HELP (Line : in String) is
+      Help_Param : String (1 .. 3) := (others => ' ');
 
-      Help_Param : String(1..3) := (others => ' ');
-    begin
+   begin
 
       if Line'LENGTH >= 3 then
-         Help_Param := Line(Line'First..(Line'First + 2));
+         Help_Param := Line (Line'First .. (Line'First + 2));
       end if;
 
-      if    Help_Param    = "COD"                        then
-            Put(CODES_HELP);
-         elsif Help_Param = "SOU"                        then
-            Put(SOURCE_HELP);
-         elsif Help_Param = "GRA"                        then
-            Put(GRAMMAR_HELP);
-         elsif Help_Param = "AGE" or Help_Param = "ERA"  then
-            Put(AGE_HELP);
-         elsif Help_Param = "ARE"                        then
-            Put(AREA_HELP);
-         elsif Help_Param = "GEO"                        then
-            Put(GEO_HELP);
-         elsif Help_Param = "FRE"                        then
-            Put(FREQ_HELP);
-         elsif Help_Param = "MEA" or Help_Param = "ABB"  then
-            Put (MEANING_HELP);
-         elsif Help_Param = "PEA"                        then
-         Put(PEARSE_HELP);
-         elsif Help_Param = "PAR" or Help_Param = "ARG"  then
-         Put(PARAM_HELP);
-         Else
-            Put(GENERAL_HELP);
-     end if;
+      if Help_Param = "COD" then
+         PUT (CODES_HELP, Output);
+      elsif Help_Param = "SOU" then
+         PUT (SOURCE_HELP, Output);
+      elsif Help_Param = "GRA" then
+         PUT (GRAMMAR_HELP, Output);
+      elsif Help_Param = "AGE" or Help_Param = "ERA" then
+         PUT (AGE_HELP, Output);
+      elsif Help_Param = "ARE" then
+         PUT (AREA_HELP, Output);
+      elsif Help_Param = "GEO" then
+         PUT (GEO_HELP, Output);
+      elsif Help_Param = "FRE" then
+         PUT (FREQ_HELP, Output);
+      elsif Help_Param = "MEA" or Help_Param = "ABB" then
+         PUT (MEANING_HELP, Output);
+      elsif Help_Param = "PEA" then
+         PUT (PEARSE_HELP, Output);
+      elsif Help_Param = "PAR" or Help_Param = "ARG" then
+         PUT (Param_Help, Output);
+      else
+         PUT (GENERAL_HELP, Output);
+      end if;
 
    exception
-      when others => null;
+      when others =>
+         null;
 
    end SHOW_HELP;
 
-end words_help;
+end Words_Help;
