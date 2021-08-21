04fa0b0764b2f000447a05171235f1b466039c29
sean rodriguez <42303133+spr93@users.noreply.github.com>
2021-08-20 23:48:15 -0700

    sync vt100 and Unicode

--- main/windows_vt100.adb
+++ main/windows_vt100.adb
@@ -1,9 +1,15 @@
+-------------------------------------
+--     NON-WINDOWS PLACEHOLDER     --
+-------------------------------------
+
+
 package body windows_vt100 is
 
-function Enable_Windows_Console_vt100_codes return boolean is
+function Enable_Windows_Console_Vt100_Codes return boolean is
 
 begin
-      pragma Compile_Time_Warning (Standard.True, "VERSION FOR NON-WINDOWS TARGETS; see windows_vt100.ads if building for Windows.");
+      pragma Compile_Time_Warning (Standard.True,
+             "VERSION FOR NON-WINDOWS TARGETS; see windows/windows_vt100.ads if building for Windows.");
 
 return false;
 
