55f097bc4a9524a6388e29591db512b29f91bb58
sean rodriguez <42303133+spr93@users.noreply.github.com>
2022-07-25 17:14:41 -0700

    add FORMAT to PREFACE Text_IO routines

--- main/preface.adb
+++ main/preface.adb
@@ -1,42 +1,48 @@
 with CONFIG;
 
-
 package body PREFACE is
 
-  procedure PUT(S : in STRING) is
+  procedure PUT (S : in String) is
   begin
-    if not CONFIG.SUPPRESS_PREFACE  then
-      TEXT_IO.PUT(TEXT_IO.CURRENT_OUTPUT, S);
+    if not CONFIG.SUPPRESS_PREFACE then
+      Text_IO.Put (Text_IO.Current_Output, S);
     end if;
   end PUT;
 
-  procedure SET_COL(PC : in TEXT_IO.POSITIVE_COUNT) is
+  procedure SET_COL (PC : in Text_IO.Positive_Count) is
   begin
-    if not CONFIG.SUPPRESS_PREFACE  then
-      TEXT_IO.SET_COL(TEXT_IO.CURRENT_OUTPUT, PC);
+    if not CONFIG.SUPPRESS_PREFACE then
+      Text_IO.Set_Col (Text_IO.Current_Output, PC);
     end if;
-  end SET_COL; 
+  end SET_COL;
 
-  procedure PUT_LINE(S : in STRING) is
+  procedure PUT_LINE (S : in String) is
   begin
-    if not CONFIG.SUPPRESS_PREFACE  then
-      TEXT_IO.PUT_LINE(TEXT_IO.CURRENT_OUTPUT, S);
+    if not CONFIG.SUPPRESS_PREFACE then
+      Text_IO.Put_Line (Text_IO.Current_Output, S);
     end if;
   end PUT_LINE;
 
-  procedure NEW_LINE(SPACING  : TEXT_IO.POSITIVE_COUNT := 1) is
+  procedure NEW_LINE (SPACING : Text_IO.Positive_Count := 1) is
   begin
-    if not CONFIG.SUPPRESS_PREFACE  then
-      TEXT_IO.NEW_LINE(TEXT_IO.CURRENT_OUTPUT, SPACING);
+    if not CONFIG.SUPPRESS_PREFACE then
+      Text_IO.New_Line (Text_IO.Current_Output, SPACING);
     end if;
   end NEW_LINE;
 
-  procedure PUT(N : in INTEGER; WIDTH : TEXT_IO.FIELD := INTEGER'WIDTH) is
-    package INTEGER_IO is new TEXT_IO.INTEGER_IO(INTEGER);
+  procedure PUT (N : in Integer; WIDTH : Text_IO.Field := Integer'Width) is
+    package INTEGER_IO is new Text_IO.Integer_IO (Integer);
   begin
-    if not CONFIG.SUPPRESS_PREFACE  then
-      INTEGER_IO.PUT(TEXT_IO.CURRENT_OUTPUT, N, WIDTH);
+    if not CONFIG.SUPPRESS_PREFACE then
+      INTEGER_IO.Put (Text_IO.Current_Output, N, WIDTH);
     end if;
   end PUT;
+
+  procedure Format (Format : in Strings_Package.Format_Command) is
+  begin
+    if not CONFIG.SUPPRESS_PREFACE then
+      STRINGS_PACKAGE.Format (TEXT_IO.CURRENT_OUTPUT, Format);
+    end if;
+  end Format;
 
 end PREFACE;
--- main/preface.ads
+++ main/preface.ads
@@ -1,13 +1,12 @@
-with TEXT_IO;
-
+with Text_IO;
+with STRINGS_PACKAGE;
 
 package PREFACE is
 
-  procedure PUT(S: in STRING);
-  procedure SET_COL(PC : in TEXT_IO.POSITIVE_COUNT);
-  procedure PUT_LINE(S : in STRING);
-  procedure NEW_LINE(SPACING  : in TEXT_IO.POSITIVE_COUNT := 1);
-  procedure PUT(N : in INTEGER; WIDTH : in TEXT_IO.FIELD := INTEGER'WIDTH);
-
+  procedure PUT (S : in String);
+  procedure SET_COL (PC : in Text_IO.Positive_Count);
+  procedure PUT_LINE (S : in String);
+  procedure NEW_LINE (SPACING : in Text_IO.Positive_Count := 1);
+  procedure PUT (N : in Integer; WIDTH : in Text_IO.Field := Integer'Width);
+  procedure Format (Format : in STRINGS_PACKAGE.Format_Command);
 end PREFACE;
-
