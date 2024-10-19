2a1a7641fef63dc2e55957e666b5a3698c146d58
sean rodriguez <42303133+spr93@users.noreply.github.com>
2024-10-19 15:11:44 -0700

    dictionary rebuild updates
    use ../obj/ directory

--- dictionary/rebuild.cmd
+++ dictionary/rebuild.cmd
@@ -1,14 +1,14 @@
-sorter D
+..\obj\sorter D
 del WORK.
-makedict G
-sorter S
+..\obj\makedict G
+..\obj\sorter S
 del WORK.
-makestem G
-makeinfl
-makeewds G
-sorter E
+..\obj\makestem G
+..\obj\makeinfl
+..\obj\makeewds G
+..\obj\sorter E
 del WORK.
-makeefil
+..\obj\makeefil
 echo Dictionary build complete.
 echo If successful, the following files constitute a complete set of dictionary data:
 echo INFLECTS.SEC ADDONS.LAT UNIQUES.LAT DICTFILE.GEN STEMFILE.GEN INDXFILE.GEN EWDSFILE.GEN
--- dictionary/rebuild.sh
+++ dictionary/rebuild.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-./sorter D
+../obj/sorter D
 # we manually delete WORK. because CREATE_FILE on WORK may cause USE_ERRROR on some older implementations.
 rm WORK.
-./makedict G
-./sorter S
+../obj/makedict G
+../obj/sorter S
 rm WORK.
-./makestem G
-./makeinfl
-./makeewds G
-./sorter E
+../obj/makestem G
+../obj/makeinfl
+../obj/makeewds G
+../obj/sorter E
 rm WORK.
-./makeefil
+../obj/makeefil
 echo "Dictionary build complete."
-echo "If successful, The following files constitute a complete set of dictionary data:"
+echo "If successful, the following files constitute a complete set of dictionary data:"
 echo "INFLECTS.SEC ADDONS.LAT UNIQUES.LAT DICTFILE.GEN STEMFILE.GEN INDXFILE.GEN EWDSFILE.GEN"
